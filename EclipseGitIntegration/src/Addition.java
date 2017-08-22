6f063f828d72125f21541dc3b17baa3c487135ba
Prateek_Gupta10 <Prateek_Gupta10@PUNITP406437D.ad.infosys.com>
2017-08-22 15:02:59 +0530

    Creating Local repository on your Desktop in GIT...

---
+++ EclipseGitIntegration/src/Addition.java
@@ -0,0 +1,15 @@
+
+public class Addition {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			// TODO Auto-generated method stub
+				int a,b,c ; //
+				a = 10 ;
+				b = 50;
+				c= a+b;
+				//System.out.println(c);
+				System.out.println("Addition Result : " + c);			
+	}
+
+}
