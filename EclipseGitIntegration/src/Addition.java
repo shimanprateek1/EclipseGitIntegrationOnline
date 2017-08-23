ad28f29a8799c80f0ec8259ac9c8b84c914fca6a
Prateek_Gupta10 <Prateek_Gupta10@PUNITP406437D.ad.infosys.com>
2017-08-23 15:43:27 +0530

    Adding code for Subtraction.

--- EclipseGitIntegration/src/Addition.java
+++ EclipseGitIntegration/src/Addition.java
@@ -4,12 +4,14 @@ public class Addition {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			// TODO Auto-generated method stub
-				int a,b,c ; //
+				int a,b,c,d ; //
 				a = 10 ;
 				b = 50;
 				c= a+b;
+				d= a-b;
 				//System.out.println(c);
 				System.out.println("Addition Result : " + c);	
+				System.out.println("Subtraction Result : " + c);	
 				System.out.println("Updating Local Addition Class ");
 				System.out.println("changes made to put a code in Online GitHUB");
 	}
